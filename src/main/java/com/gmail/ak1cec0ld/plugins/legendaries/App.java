67ee8792a4972bf022c0e8021f479c061b30f186
Jeremy Luchak <jeremy@nevada.unr.edu>
2019-04-16 09:22:56 -0700

    initial setup

---
+++ src/main/java/com/gmail/ak1cec0ld/plugins/legendaries/App.java
@@ -0,0 +1,13 @@
+package com.gmail.ak1cec0ld.plugins.legendaries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
